ac6d994c0f39831bf87459fb2fbd565da3357fa6
Maffey <matiusz98@gmail.com>
2017-07-04 19:15:08 +0200

    Changed the connectivity check manager to be in the onClick method; Changed the loading bar to be set to invisible rather than gone, so it can be used again when we do multiple searches.

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -19,11 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderCallbacks<List<Book>> {
-    /**
-     * TODO: IMPLEMENT MAIN URL THAT WILL SHOW MOST POPULAR BOOKS. CREATE A METHOD THAT WILL
-     * TODO: CREATE A NEW URL LINK FROM STRING THAT USER PUTS INTO THE EDIT TEXT.
-     * TODO: POLISH LAYOUT AND EXPAND CLASS.
-     **/
+
     private static final String LOG_TAG = MainActivity.class.getName();
 
     /** TextView that is displayed when the list is empty */
@@ -38,11 +34,11 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
      * Constant value for the book loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
      */
-    private static final int EARTHQUAKE_LOADER_ID = 0;
+    private static final int BOOK_LOADER_ID = 0;
 
-    /** URL for earthquake data from the USGS dataset */
+    /** URL for earthquake data from the dataset */
     private static String BOOKS_REQUEST_URL =
-            "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=20";
+            "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=30";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         mEmptyView = (TextView) findViewById(R.id.empty_view);
         bookListView.setEmptyView(mEmptyView);
         bar = (ProgressBar) findViewById(R.id.loading_view);
+        bar.setVisibility(View.INVISIBLE);
 
         //Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
@@ -66,25 +63,12 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         ConnectivityManager cm =
                 (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        boolean isConnected = activeNetwork != null &&
+        final boolean isConnected = activeNetwork != null &&
                 activeNetwork.isConnectedOrConnecting();
-        if (isConnected) {
-            // Get a reference to the LoaderManager, in order to interact with loaders.
-            LoaderManager loaderManager = getLoaderManager();
-
-            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-            // because this activity implements the LoaderCallbacks interface).
-            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        } else {
-            bar.setVisibility(View.GONE);
-            mEmptyView.setText(R.string.no_connection);
-        }
 
         // Setting onClickListener to make search toolbar work.
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
-            // TODO: MAKE SURE IT WORKS.
             @Override
             public void onClick(View view) {
                 EditText editText = (EditText) findViewById(R.id.edit_text);
@@ -92,19 +76,35 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
                 String baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
                 String freshUrl = baseUrl + userInput;
                 BOOKS_REQUEST_URL = freshUrl.replaceAll(" ","+");
+
+                // Get a reference to the LoaderManager, in order to interact with loaders.
+                LoaderManager loaderManager = getLoaderManager();
+                if (loaderManager.getLoader(BOOK_LOADER_ID) != null) {
+                    loaderManager.restartLoader(BOOK_LOADER_ID, null, MainActivity.this);
+                }
+                if (isConnected) {
+                    // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+                    // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+                    // because this activity implements the LoaderCallbacks interface).
+                    loaderManager.initLoader(BOOK_LOADER_ID, null, MainActivity.this);
+                } else {
+                    bar.setVisibility(View.INVISIBLE);
+                    mEmptyView.setText(R.string.no_connection);
+                }
             }
         });
     }
 
     @Override
     public Loader<List<Book>> onCreateLoader(int i, Bundle bundle) {
+        bar.setVisibility(View.VISIBLE);
         return new BookLoader(this, BOOKS_REQUEST_URL);
     }
 
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
         // Handling fluid user experience by setting empty_view and loading indicator.
-        bar.setVisibility(View.GONE);
+        bar.setVisibility(View.INVISIBLE);
 
         mEmptyView.setText(R.string.empty_view);
         // Clear the adapter of previous earthquake data
@@ -119,6 +119,7 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
     @Override
     public void onLoaderReset(Loader<List<Book>> loader) {
+        bar.setVisibility(View.VISIBLE);
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
     }
